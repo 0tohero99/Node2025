38881a37d5bef5ba5c586ac5674e8a9c25452906
0tohero99 <shubhamsinghhindustan@gmail.com>
2025-02-19 19:40:13 -0800

    .

--- inbuilt/static.js
+++ inbuilt/static.js
@@ -1,14 +1,17 @@
-const { error } = require("console");
 let fs = require("fs");
 let http = require("http");
 
+
 let server = http.createServer((req, res) => {
-    // reading json file .//
+    //reading file .
     fs.readFile("city.json", "utf-8", (error, data) => {
-        if (error) throw error
-        // returning file .//
+        if (error) throw error;
+        //  returning  data .
         res.write(data);
         res.end()
     })
 })
-server.listen(7001)
+server.listen(7000, (error) => {
+    if (error) throw error;
+    console.log("Server is running on port 7000");
+})
